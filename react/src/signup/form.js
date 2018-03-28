2994df3d08bd3b65f66b742fc2980c46aa10462b
dbrudner <dbrudne1@gmail.com>
2018-03-28 01:12:40 -0400

    commit

--- react/src/signup/form.js
+++ react/src/signup/form.js
@@ -1,44 +1,108 @@
 import React, {Component} from 'react'
-import axios from 'axios'
 import styled from 'styled-components'
+import axios from 'axios'
 
-export default class Form extends Component {
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import { Login, openSignUp } from '../actions/index';
+
+const FormContainer = styled.div``
+
+const Label = styled.span`
+    margin-right: 3rem;
+`
+
+const SubmitButton = styled.button``
+
+
+class Form extends Component {
 
     constructor(props) {
         super(props)
 
-        this.state= {
-            username: '',
-            email: '',
-            password: ''
+        this.state = {
+            user: {
+                username: '',
+                password: '',
+                password_confirm: '',
+                
+            },
+            error: null,
+            redirectTo: null
         }
     }
 
-    handleChange = (name, value) => {
-        this.setState({[name]: value}, () => {
-            console.log(this.state)
+    renderInput = name => {
+        let label = name.charAt(0).toUpperCase() + name.substr(1)
+        label = label.replace('_', ' ')
+
+        return (
+            <div>
+                <Label>
+                    {label}
+                </Label>
+                <input key={name} value={this.state.user[name]} type='text' onChange={event => this.handleChange(name, event.target.value)} />
+            </div>
+        )
+    }
+
+    renderAllInputs = () => {
+        return Object.keys(this.state.user).map(item => {
+            return <div key={item}>{this.renderInput(item)}</div>
         })
     }
 
-    handleSubmit = event => {
-        event.preventDefault();
-        const username = this.state.username.trim()
-        const email = this.state.email.trim()
-        const password = this.state.password.trim()
+    handleChange = (name, value) => {this.setState({user: {...this.state.user, [name]: value}})}
 
-        if (username && email && password) {
-            axios.post('/signup', {username, email, password})
+    handleSubmit = event => {
+        event.preventDefault()
+        if (this.state.password === this.state.password_confirm) {
+            return this.setState({error: "Passwords don't match"})
         }
+
+        const user = this.state.user
+
+        axios.post('/signup', user)
+        .then(res => {
+            console.log('sign up success')
+            axios.get('/test')
+            .then(res => {
+                this.props.Login(res.data)
+                this.props.closeModal()
+            })
+
+        }).catch (err => {
+            this.setState({
+                error: 'Username taken'
+            })
+            throw err
+        })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input value={this.state.username} type='text' onChange={event => {this.handleChange('username', event.target.value)}}/>
-                <input value={this.state.email} type='text' onChange={event => {this.handleChange('email', event.target.value)}}/>
-                <input value={this.state.password} type='password' onChange={event => {this.handleChange('password', event.target.value)}}/>
-                <button type='submit'>Button</button>
-            </form>
+            <FormContainer>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderAllInputs()}
+                    <div>
+                        {this.state.error || null}
+                    </div>
+                    <SubmitButton>Submit</SubmitButton>
+                </form>
+            </FormContainer>
         )
     }
 }
+
+
+function mapStateToProps(state) {
+    return {
+        state
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({Login, openSignUp}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form)
